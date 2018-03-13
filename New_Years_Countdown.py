c5ab68fe8ab7f60677a52a179a4efbc569677745
David Halvorsen <daviqd.halvorsen@gmail.com>
2018-03-13 16:57:11 -0700

    Create New_Years_Countdown.py

---
+++ New_Years_Countdown.py
@@ -0,0 +1,9 @@
+# This is a demonstration of function definitions, tabs and range
+# This program runs in Python 2.6.
+def BallDrop():
+    print "New Years is quickly approaching!"
+    print "I hope you're ready to make a resolution ;)"
+    for number in range(10, 0, -1):
+        print "\t\t %d" % number
+    print "\tHAPPY NEW YEAR!"
+BallDrop()
